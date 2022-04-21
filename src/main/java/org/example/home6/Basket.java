9b8ae3ff7fb673194e1a9029a9eca7abb7908b07
glumovdenis <den.glumov@gmail.com>
2022-04-22 01:01:45 +0700

    Homework7

--- src/main/java/org/example/home6/Basket.java
+++ src/main/java/org/example/home6/Basket.java
@@ -1,5 +1,6 @@
 package org.example.home6;
 
+import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -7,12 +8,13 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class Basket extends BasePage{
+    private final static String LIKE_BUTTON_CLOSE_XPATH_LOCATOR = "//div[@data-widget='alertPopup']//button[.='']";
 
     public Basket(WebDriver driver) {
         super(driver);
     }
 
-    @FindBy(xpath = "//span[@class=\'ui-b4 ui-c5 ui-e8\']")
+    @FindBy(xpath = LIKE_BUTTON_CLOSE_XPATH_LOCATOR)
     private WebElement closeWindow;
 
     @FindBy(xpath = "//div[@data-widget='column']/div[@class]/span")
@@ -21,8 +23,9 @@ public class Basket extends BasePage{
     @FindBy(xpath = "(//span[.='Удалить']/ancestor::button)[2]")
     private WebElement deteteGoods;
 
+    @Step("Работа с корзиной")
     public void setDeleteSelectGoods(){
-        webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[@class=\'ui-b4 ui-c5 ui-e8\']")));
+        webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(LIKE_BUTTON_CLOSE_XPATH_LOCATOR)));
         closeWindow.click();
         deleteSelectGoods.click();
         deteteGoods.click();
--- src/main/java/org/example/home6/Catalog.java
+++ src/main/java/org/example/home6/Catalog.java
@@ -1,5 +1,6 @@
 package org.example.home6;
 
+import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -7,16 +8,18 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class Catalog extends BasePage{
+    private final static String LIKE_DIV_XPATH_LOCATOR = "//div[@data-widget='searchResultsV2']/div/div[@class][1]/a";
 
     public Catalog(WebDriver driver) {
         super(driver);
     }
 
-    @FindBy(xpath = "//div[@data-widget='searchResultsV2']/div/div[@class][1]/a")
+    @FindBy(xpath = LIKE_DIV_XPATH_LOCATOR)
     public WebElement nameGoods;
 
+    @Step("Выбор товара")
     public Goods setNameGoods() {
-        webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@data-widget='searchResultsV2']/div/div[@class][1]/a")));
+        webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(LIKE_DIV_XPATH_LOCATOR)));
         nameGoods.click();
         return new Goods(driver);
     }
